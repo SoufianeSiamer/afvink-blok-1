5e41af15cbcf8246cba2dc7d90437a71aeb59b16
SoufianeSiamer <55231577+SoufianeSiamer@users.noreply.github.com>
2020-03-01 23:34:48 +0100

    Add files via upload

--- afvink 6.py
+++ afvink 6.py
@@ -1,15 +1,43 @@
-from typing import List, Any, Union
 
 
-def lottery():
-    from random import seed
-    from random import randint
-    seed(100)
-    nummers= []
-    for i in range(10):
-        random= randint(1, 10)
-        for t in range(1):
-            nummers.append(random)
-    print(nummers)
-lottery()
+seq = "ACTAGCAACCTCAAACAGACACCATGGTGCACCTGACTCCTGTGGAGAAGTCTGCCGTTACTGCCCTGTGGGGCAAGGTGAACGTGGATGAAGTTGGTGGTGAGGCC"
+#Opent de enzymen bestand, verwijderd het dakje.
+def enzymen():
+    knipenzymen = {}
+    bestand = open ("enzymen.txt")
+    for regel in bestand:
+        enzym_name, seq = regel.split()
+        seqenzym = seq.replace("^","")
+        knipenzymen[enzym_name] = seqenzym
+    return knipenzymen
+    
+# zoekt waar het enzym kan knippen
+def zoek(seq, eiwit, enzym):
+    o = ""
+    index = 0
+    plaats = []
+    while index < len(seq):
+        index = seq.find(eiwit, index)
+        if index == -1:
+              break
+        o = o+" " * (index - len(o)) + str(eiwit)
+        ff = index
+        plaats.append(index)
+        index += len(eiwit)
+        
+    return o,plaats
 
+
+
+def match():
+#matcht het enzym op de goeie plek
+    knipenzymen = enzymen()
+    result = {}
+    for enzym in knipenzymen:
+        if seq.find(knipenzymen[enzym]) != -1:
+            o,plaats = zoek(seq, knipenzymen[enzym], enzym)
+            print ("er is een match gevonden met " + str (enzym) + " op positie(s)" + str (plaats))
+            print (seq)
+            print(o)
+            
+match()
---
+++ afvink 7.py
@@ -0,0 +1,40 @@
+list_ = []
+#openen van het bestand en in een lijst zetten
+def openbestand_():
+    bestand = open("SC_expression.csv")
+    bestand.readline()
+    
+    for regel in range(6071):
+        regel = bestand.readline()
+        regel = regel.strip()
+        regel = regel.split(",")
+        list_.append(regel)
+
+
+
+#gemiddelde berekenen van een kolom
+def gemiddelde_():
+    kolom = int(input ("Welke kolom wil je hebben?"))
+    gem = 0
+    for getal in range(len(list_)):
+        gem += float (list_[getal][kolom])
+          
+    gem = gem / len(list_)
+    print (gem)
+
+
+#bekijken welke values boven 50 zijn
+def hogerdan():
+    kolom = int(input ("Welke kolom wil je hebben?"))
+    for getal in range(len(list_)):
+        waarde = float (list_[getal][kolom])
+        if waarde > 50 or waarde == 50:
+            print (list_[getal][0])
+        
+
+
+def main():
+    openbestand_()
+    gemiddelde_()
+    hogerdan()
+main()
